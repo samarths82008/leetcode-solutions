bc03aad81c72576496d707992fe371a5457fa5c3
Vijay Singh <vsingh@ischool.berkeley.edu>
2022-08-06 13:24:24 -0700

    add binary

---
+++ addbinary.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :2nbtype b: str
+        :rtype: str
+        """
+        sum = bin(int(a, 2) + int(b, 2))
+        return sum[2:]        
