7a1e7ffc7a19df32c9da8fd074594b315e0c8a50
Vijay Singh <vsingh@ischool.berkeley.edu>
2022-08-06 13:06:13 -0700

    Plus One solution

---
+++ plusonesolution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        i = len(digits) - 1
+        while digits[i] == 9:
+            digits[i] = 0
+            i -= 1
+        if i < 0:
+            digits = [1] + digits
+        else:
+            digits[i] += 1
+        return digits
+            
